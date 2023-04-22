b092f24694b9f06fce767c678e9a138f78ea742b
Adenyson <Adenyson@outlook.com>
2023-04-22 20:50:33 -0300

    Update Connection.js

--- src/MongoDB/Connection.js
+++ src/MongoDB/Connection.js
@@ -1,14 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-
-// URL de conexão com o banco de dados
-let url = `mongodb+srv://Adenyson:PnFuCYNRlM0w3UbM@cluster0.crv7oxf.mongodb.net/test`
-
+require('dotenv').config();
 
 // Nome do banco de dados
 const dbName = 'BOT_DISCORD_LASTFM';
 
 // Cria um novo cliente do MongoDB
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Conecta ao banco de dados e obtém uma referência para o objeto db
 async function connect() {
